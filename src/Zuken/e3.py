99ea7232b5129d27bcd69a8aed6bcb670b21e095
rory652 <rory.coe652@gmail.com>
2023-12-14 10:52:12 +0000

    Added more docstrings

--- src/Zuken/e3.py
+++ src/Zuken/e3.py
@@ -21,21 +21,63 @@ def dispatch(app_name: str):
     return app
 
 def print(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutInfo" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
     E3.PutInfo(popup, msg, item)
 
 def warn(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutWarning" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
     E3.PutWarning(popup, msg, item)
 
 def error(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutError" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
     E3.PutError(popup, msg, item)
 
 def message(msg, item=0):
+    """
+    Shorthand for the "PutMessage" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        item: ID of an item in the project (allows you to jump to it)
+    """
     E3.PutMessage(msg, item)
 
 def verify(msg, popup=0, item=0):
+    """
+    Shorthand for the "PutVerify" method
+
+    Args:
+        msg: Message to display (gets converted to string automatically, so anything works)
+        popup: Whether to display an additional dialogue or not
+        item: ID of an item in the project (allows you to jump to it)
+    """
     E3.PutVerify(popup, msg, item)
 
 E3 = dispatch("CT.Application")
+"""E3 Object - Links to all instances of the application"""
+
 Job = E3.CreateJobObject()
+"""E3 Job Object - Links to a single instance of the application"""
 
 
